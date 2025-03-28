21effb78a438b9817ccf3fd913526ac70734fc7b
xy297180 <39511359+xy297180@users.noreply.github.com>
2025-03-28 10:49:39 +0800

    Add files via upload

---
+++ run.js
@@ -0,0 +1,16 @@
+import Fontmin from 'fontmin';
+
+
+const fontmin = new Fontmin()
+  .src("./fonts/AlimamaShuHeiTi-Bold/AlimamaShuHeiTi-Bold.ttf") // 源字体文件
+  .use(Fontmin.glyph({ text: "任务同步平台总览管理创建" })) // 只保留这些字符
+  .use(Fontmin.ttf2woff2()) // 转换为 WOFF2 格式
+  .dest("output/"); // 输出目录
+
+fontmin.run((err, files) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("字体优化完成！");
+  }
+});
